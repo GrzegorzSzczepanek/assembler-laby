e40eaf99a690acca1ac669fc5914e4d19e672287
GrzegorzSzczepanek <grzegorzszczepanek48@gmail.com>
2025-05-19 00:55:33 +0200

    sprawozdanie

--- lab5/test.cpp
+++ lab5/test.cpp
@@ -7,12 +7,12 @@ extern "C" double dodaj2(double a, double b);
 extern "C" int testowa();
 
 int main() {
-    printf("Hello world\n");
-    int result = testowa();
-    int suma = dodaj(21, 37);
-    double suma_double = dodaj2(21.5, 37.3);
-    printf("Testowa funkcja return: %d\n", result);
-    printf("Dodaj funkcja return: %d\n", suma);
-    printf("Dodaj2 funkcja return: %f\n", suma_double);
-    return 0;
+  printf("Hello world\n");
+  int result = testowa();
+  int suma = dodaj(21, 37);
+  double suma_double = dodaj2(21.5, 37.3);
+  printf("Testowa funkcja return: %d\n", result);
+  printf("Dodaj funkcja return: %d\n", suma);
+  printf("Dodaj2 funkcja return: %f\n", suma_double);
+  return 0;
 }
